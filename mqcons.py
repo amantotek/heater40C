cb6f0a74f2da32c64f165a940ffb4fa789a02c26
amantotek <7591528+amantotek@users.noreply.github.com>
2026-08-09 15:13:13 +0100

    mqcons.py

---
+++ mqcons.py
@@ -0,0 +1,8 @@
+gsFilNom = "mqcons.py" #Written in MicroPython for ESP32 WROOM
+gsVEERSN = gsFilNom + " V001" # Add send to MQTT server
+
+sBrokerHost = "xxxxxxxxxxxxxxxxxxxxxxxx.s1.eu.hivemq.cloud"
+iBrokerPort = 8883
+sMqttUser = "yourUser"
+sMqttPassword = "yourPass"
+# ----END----
